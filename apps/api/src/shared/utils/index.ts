f76b56561e298f5625a017f0456924d2bf5f5c20
David Kapuza <kapuzadavid@gmail.com>
2025-02-02 15:28:46 +0100

    chore(api): unused util

--- apps/api/src/shared/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
-import crypto from 'crypto';
-
-export function getHash() {
-  return crypto
-    .createHash('sha256')
-    .update(randomStringGenerator())
-    .digest('hex');
-}
