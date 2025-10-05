677a7d4c70a0a0ceb993d6149ca0a913ee2b60ff
David Kapuza <kapuzadavid@gmail.com>
2025-10-05 22:36:03 +0200

    feat(ui): initialize UI component library with button component and Tailwind CSS integration

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,6 +1,7 @@
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
+import { Button } from '@phialon/ui';
 
 export function App() {
   return (
@@ -16,7 +17,7 @@ export function App() {
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className='p-2 text-white bg-blue-500 rounded'>Home</Link>
           </li>
           <li>
             <Link to="/page-2">Page 2</Link>
@@ -29,6 +30,7 @@ export function App() {
           element={
             <div>
               This is the generated root route.{' '}
+              <Button>Phialon Button</Button>{' '}
               <Link to="/page-2">Click here for page 2.</Link>
             </div>
           }
---
+++ packages/ui/README.md
@@ -0,0 +1,7 @@
+# @phialon/ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @phialon/ui` to execute the unit tests via [Jest](https://jestjs.io).
